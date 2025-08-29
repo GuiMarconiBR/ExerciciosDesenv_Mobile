ec6123973d688a1452ed8ab7f1a761d661cfd6fc
Guilherme Marconi <161528509+GuiMarconiBR@users.noreply.github.com>
2025-08-29 20:46:54 -0300

    Update dardo.ts
    Finalizado o exercício dos dardos

--- src/02-conditional/dardo.ts
+++ src/02-conditional/dardo.ts
@@ -3,3 +3,17 @@
 Você deve criar um programa para, dadas as medidas das três tentativas de lançamento, informar qual 
 foi a maior.*/
 
+import promptSync from 'prompt-sync';
+const prompt = promptSync();
+
+const a1 = Number(prompt('Primeiro arremesso: '));
+const a2 = Number(prompt('Segundo Arremesso: '));
+const a3 = Number(prompt('Terceiro Arremesso: '));
+
+if (a1 > a2 && a1 > a3 || a1 == a2 && a1 != a3) {
+    console.log(`Maior distância: ${a1}`)
+} else if (a2 > a3 && a2 > a1 || a2 == a1 && a2 != a3) {
+    console.log(`Maior distância: ${a2}`)
+} else {
+    console.log(`Maior distância: ${a3}`)
+};
